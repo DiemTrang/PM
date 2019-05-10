54ecc7e4eb955639b2dcf41a37288c82a480afee
Diemtrang <nguyenthidiemtrang1996@gmail.com>
2019-05-10 12:53:38 +0700

    adđ

---
+++ src/main/java/com/project/bll/UserService.java
@@ -0,0 +1,16 @@
+package com.project.bll;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service(value = "userService")
+@Transactional
+public class UserService {
+	// region -- Fields --
+
+	// end
+
+	// region -- Methods --
+
+	// end
+}
---
+++ src/main/java/com/project/dal/UserDao.java
@@ -0,0 +1,5 @@
+package com.project.dal;
+
+public interface UserDao {
+
+}
