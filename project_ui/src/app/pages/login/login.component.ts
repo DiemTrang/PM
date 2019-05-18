a28f4fcadd9329500844778c278be1bcea857775
hoannguyen <nvhoanbk14@gmail.com>
2019-05-18 23:15:32 +0700

    Login giao diện, chưa có API

--- project_ui/src/app/pages/login/login.component.ts
+++ project_ui/src/app/pages/login/login.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ModalDirective } from 'ngx-bootstrap';
+import { FormGroup, FormBuilder, AbstractControl } from '@angular/forms';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-login',
@@ -7,14 +11,68 @@ import { ModalDirective } from 'ngx-bootstrap';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public router: Router;
+  public form: FormGroup;
+  public email: AbstractControl;
+  public password: AbstractControl;   
+  public vm: any = { email: "", userName: "", password: "" }
 
   @ViewChild("forgotPassModal") public forgotPassModal: ModalDirective;
-  
+
   public show = false;
 
-  constructor() { }
+  constructor(router: Router, fb: FormBuilder) {
+    this.router = router;
+    this.form = fb.group({
+      'email': 'hoan',
+      'password': '123'
+    });
+
+    this.email = this.form.controls['userName'];
+    this.password = this.form.controls['password'];
+  }
 
   ngOnInit() {
   }
 
+  public onSubmit(): void {
+    if (this.form.valid) {
+      alert("Hello");
+      this.router.navigate(['/profile']);
+    }
+    else {
+      alert('Sai Username hoặc Password');
+    }
+  }
+
+  public signIn() {
+    //document.getElementById('preloader').style.display = 'block';
+
+
+    let obj = {
+        email: this.vm.userName,
+        password: this.vm.password
+    };
+    if(this.vm.userName == 'hoan' && this.vm.password == '123'){
+      this.router.navigate(['/profile']);
+    }
+    else {
+      alert("Dẹp me di");
+      alert("Đùa thôi. Username: Hoan, Password: 123");
+    }
+    // this.pro.signIn(obj).subscribe((rsp: any) => {
+    //     if (rsp.status === HTTP.STATUS_SUCCESS) {
+    //         this.message = "";
+    //         this.pro.saveAuth(rsp.result);
+    //     } else {
+    //         this.showMsg = true;
+    //         this.message = " Invalid User ID or Password";
+    //     }
+    //     this.loader = false;
+    // }, (err) => { console.log(err); });
+
+    setTimeout(function () {
+        //document.getElementById('preloader').style.display = 'none';
+    }, 500);
+}
 }
--- project_ui/src/app/pages/login/login.module.ts
+++ project_ui/src/app/pages/login/login.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalModule } from 'ngx-bootstrap/modal';
+import { RouterModule } from '@angular/router';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import {LoginComponent} from './login.component';
 import {LoginRoutingModule} from './login-routing.module';
+
+
+export const routes = [
+  { path: '', component: LoginComponent, pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [
     CommonModule,
     LoginRoutingModule, 
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(routes),
     ModalModule.forRoot()
   ],
   declarations: [
