342a163186349432b0b68b94b280f8e457091b18
Diemtrang <nguyenthidiemtrang1996@gmail.com>
2019-05-29 16:27:56 +0700

    Update SQL

--- src/main/resources/sql/TaskDao/getTaskDetail.sql
+++ src/main/resources/sql/TaskDao/getTaskDetail.sql
@@ -1,7 +1,23 @@
 SELECT 
-	a.id, a.title project, b.title, b.status, c."name" 
-FROM "public".project a 
-JOIN task b 
-	ON b.project = a.id 
-JOIN "public"."user" c 
-	ON b.asign = c."id"
+	a.id, b.title project, a.title, a.status, c."name" asign, a.priority, a.due_date, 
+	a.original_estimate, a.decription, a.start_date, a.end_date, d.createBy, 
+	a.create_on, e.modifyBy 
+FROM task a 
+JOIN project b 
+	ON a.project = b.id 
+JOIN "public".user c 
+	ON a.asign = c.id 
+JOIN(SELECT 
+		a.id, a.title, c.name createBy 
+	FROM task a 
+	JOIN "public".user c 
+		ON a.create_by = c.id 
+	)d 
+ON a.id = d.id 
+JOIN(SELECT 
+		a.id, a.title, c.name modifyBy 
+	FROM task a 
+	JOIN "public".user c 
+		ON a.modify_by = c.id 
+	)e 
+ON a.id = e.id 
