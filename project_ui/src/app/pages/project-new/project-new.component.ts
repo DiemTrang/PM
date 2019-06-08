61c4582164ba5e4c7656cc0a71a51651c56b1325
Diemtrang <nguyenthidiemtrang1996@gmail.com>
2019-06-08 14:11:11 +0700

    TẠo mới Project ok

--- project_ui/src/app/pages/project-new/project-new.component.ts
+++ project_ui/src/app/pages/project-new/project-new.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ProjectProvider } from 'src/app/providers';;
 import { HTTP } from 'src/app/utilities';
 import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ModalDirective } from 'ngx-bootstrap';
 
 @Component({
   selector: 'app-project-new',
@@ -11,6 +12,11 @@ import { ActivatedRoute, Params, Router } from '@angular/router';
 export class ProjectNewComponent implements OnInit {
   public data: any = {};
 
+  public title = '';
+    public msgInfo = '';
+    public modalState = '';
+
+    @ViewChild('discardModal') public discardModal: ModalDirective;
   constructor(
     private pro: ProjectProvider,
     private act: ActivatedRoute,
@@ -23,6 +29,10 @@ export class ProjectNewComponent implements OnInit {
     document.getElementById('preloader').style.display = 'block';
     this.pro.createProject(this.data).subscribe((rsp: any) => {
         if (rsp.status === HTTP.STATUS_SUCCESS) {
+          this.title = 'Information';
+          this.msgInfo = 'New Customer Limit request is created, successfully.';
+          this.modalState = 'success';
+          this.discardModal.show();
             return;
         }
     }, (err) => { console.log(err); });
